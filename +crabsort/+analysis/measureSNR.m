44ac8a48d681833d21bc2be2e4afd5adc7bec941
gitlab@srinivas.gs <code@srinivas.gs>
2019-06-14 15:14:00 -0400

    fixed #218

---
+++ +crabsort/+analysis/measureSNR.m
@@ -0,0 +1,67 @@
+function data = measureSNR(file_name, data)
+
+load([file_name.folder filesep file_name.name],'-mat','crabsort_obj')
+
+disp(file_name.name)
+
+self = crabsort(false);
+
+self.path_name = file_name.folder;
+self.file_name = strrep(file_name.name,'.crabsort','');
+
+self.loadFile;
+
+% window size to average over the "noise"
+N = round(.1/self.dt);
+
+nerve_names = self.common.data_channel_names;
+
+if isempty(self.spikes)
+	return
+end
+
+fn = fieldnames(self.spikes);
+
+for i = 1:(self.raw_data_size(2))
+	if isempty(nerve_names{i})
+		continue
+	end
+
+	nerve = nerve_names{i};
+
+	if ~any(strcmp(fn,nerve))
+		continue
+	end
+
+	neuron_names = fieldnames(self.spikes.(nerve));
+
+	for j = 1:length(neuron_names)
+
+		spike_locations = self.spikes.(nerve).(neuron_names{j});
+
+
+		mean_spike_ht = abs(mean(self.raw_data(spike_locations,i)));
+
+		try
+			temp = reshape(self.raw_data(:,i),N,round(self.raw_data_size(1)/N));
+		catch
+			continue
+		end
+
+		rm_this = unique(round(spike_locations*self.dt*10));
+		rm_this(rm_this == 0) = [];
+		temp(:,rm_this) = [];
+		temp(1:round(N/2),:) = [];
+		noise_sigma =  mean(std(temp));
+
+		% save all of this
+		data.file_name = [data.file_name; self.file_name];
+		data.nerve_name = [data.nerve_name; nerve];
+		data.neuron_name = [data.neuron_name; neuron_names{j}];
+		data.SNR = [data.SNR; (mean_spike_ht/noise_sigma)];
+
+	end
+
+
+
+end
---
+++ +crabsort/SNR.m
@@ -0,0 +1,99 @@
+% measures the signal-to-noise ratio in sorted data
+
+function data = SNR(varargin)
+
+
+
+% options and defaults
+options.DataDir = pwd;
+options.MakePlot = false;
+
+% validate and accept options
+options = corelib.parseNameValueArguments(options,varargin{:});
+
+allfiles = dir([options.DataDir filesep '*.crabsort']);
+
+if isempty(allfiles)
+	error('No data found')
+end
+
+
+
+% load the common data
+load([allfiles(1).folder filesep 'crabsort.common'],'-mat','common')
+
+
+
+
+
+data = struct;
+N = length(allfiles);
+data.file_name = categorical(repmat(NaN,10*N,1));
+data.nerve_name = categorical(repmat(NaN,10*N,1));
+data.neuron_name = categorical(repmat(NaN,10*N,1));
+data.SNR = (repmat(NaN,10*N,1));
+
+
+temp_data = struct;
+for i = 1:N
+	temp_data(i).file_name = categorical({''});
+	temp_data(i).nerve_name = categorical({''});
+	temp_data(i).neuron_name = categorical({''});
+	temp_data(i).SNR = NaN;
+end
+
+
+
+% load all the data into the data structure
+% in parallel
+parfor i = 1:N
+	temp_data(i) = crabsort.analysis.measureSNR(allfiles(i), temp_data(i));
+end
+
+
+% reshape
+data.SNR = vertcat(temp_data.SNR);
+data.neuron_name = vertcat(temp_data.neuron_name);
+data.nerve_name = vertcat(temp_data.nerve_name);
+data.file_name = vertcat(temp_data.file_name);
+
+
+% clean up
+rm_this = isnan(data.SNR);
+
+data.SNR(rm_this) = [];
+data.neuron_name(rm_this) = [];
+data.nerve_name(rm_this) = [];
+data.file_name(rm_this) = [];
+
+if ~options.MakePlot
+	return
+end
+
+figure('outerposition',[300 300 1200 600],'PaperUnits','points','PaperSize',[1200 600]); hold on
+
+
+unique_nerves = unique(data.nerve_name);
+L = {};
+idx = 1;
+for i = 1:length(unique_nerves)
+	unique_neurons = unique(data.neuron_name(data.nerve_name == unique_nerves(i)));
+	for j = 1:length(unique_neurons)
+		use_this = data.nerve_name == unique_nerves(i) & data.neuron_name == unique_neurons(j);
+
+		y = data.SNR(use_this);
+		plot(0*y+idx+.01*randn(length(y),1),y,'o');
+		L{end+1} = [char(unique_nerves(i)) '/' char(unique_neurons(j))];
+
+		text(idx-.1,max(y)*1.2,strlib.oval(max(y)),'FontSize',24)
+		idx = idx + 1;
+	end
+
+
+end
+
+ylabel('SNR')
+ax = gca;
+set(gca,'YScale','log','YGrid','on','XLim',[0 idx],'XTick',[1:idx-1],'XTickLabel',L,'YLim',[ax.YLim(1)*.75 ax.YLim(2)*1.5])
+title(char(data.file_name(1)),'interpreter','none')
+figlib.pretty()
