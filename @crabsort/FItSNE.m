8e55a0cfd6e7a79656f161fbcb0090fbe6181118
Alec Hoyland <alec.hoyland@posteo.net>
2019-06-19 13:37:12 -0400

    Add documentation

--- @crabsort/FItSNE.m
+++ @crabsort/FItSNE.m
@@ -9,12 +9,23 @@
 %  \___|_|  \__,_|_.__/|___/\___/|_|   \__|
 %
 % this is a plugin for crabsort.m
-% clusters using fast Fourier transform-interpolated t-distributed stochastic neighborhood embedding
+% clusters using fast Fourier transform-interpolated t-distributed stochastic neighborhood embedding (FIt-SNE)
 % https://github.com/KlugerLab/FIt-SNE
 %
 % created by Alec Hoyland at 13:25 19 June 2019
 % contact me at entropyvsenergy@posteo.de
 %
+% FIt-SNE requires a separate package
+% 1. Download the package from: https://github.com/KlugerLab/FIt-SNE
+%     or clone using git clone https://github.com/KlugerLab/FIt-SNE
+% 2. Add the downloaded folder to your MATLAB path
+%     >> addpath path/to/FIt-SNE
+%     >> savepath
+% 3. Download FFTW from http://www.fftw.org/ or get it for your operating system/distribution
+% 4. From the root directory of FIt-SNE, run:
+%     $ g++ -std=c++11 -O3  src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp  -o bin/fast_tsne -pthread -lfftw3 -lm
+%     If you are using Windows, instead go to https://github.com/KlugerLab/FIt-SNE and download the compiled binary
+% 
 
 function FItSNE(self)
 
