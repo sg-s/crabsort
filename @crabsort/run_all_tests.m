ed7f883642218543232ac098cfff09a0773feeef
Srinivas Gorur-Shandilya <code@srinivas.gs>
2019-09-11 20:40:22 -0400

    fixed #250

--- @crabsort/run_all_tests.m
+++ @crabsort/run_all_tests.m
@@ -3,6 +3,23 @@
 function [passed,total] = run_all_tests()
 
 
+% check that we know where the compiled binary of md5hash is
+a = which('hashlib.md5hash');
+assert(~isempty(a),'md5hash not found on path, so cannot continue')
+[folder_name, ~, ext] = fileparts(a);
+
+assert(strcmpi(['.' mexext],ext),'md5hash does not point to compiled binary')
+
+% make sure that all 3 compiled binaries are there -- useful for 
+% testing on one platform and being sure it works on another
+allfiles = dir(folder_name);
+
+assert(any(strcmp({allfiles.name},'md5hash.mexa64')),'GNU/Linux binary missing')
+assert(any(strcmp({allfiles.name},'md5hash.mexw64')),'Windows binary missing')
+assert(any(strcmp({allfiles.name},'md5hash.mexmaci64')),'macOS binary missing')
+
+
+
 passed = 0;
 failed = 0;
 
